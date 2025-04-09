e07c8fc7766fe268e4d61b2ee53352a6d962111e
Le Nguyen Hung <lnhungtn789@gmail.com>
2025-04-09 13:19:56 +0700

    fix docker file

---
+++ Back-end/src/main/java/com/hung/shop/controllers/ProductController.java
@@ -0,0 +1,14 @@
+package com.hung.shop.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products/")
+public class ProductController {
+    @GetMapping()
+    public String getAllProducts() {
+        return "List of products";
+    }
+}
