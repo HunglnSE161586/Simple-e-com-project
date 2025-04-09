42bb4f05f95f8fd2771ad75ec8ffb09e27527b1f
Le Nguyen Hung <lnhungtn789@gmail.com>
2025-04-09 17:18:19 +0700

    add entities: Orders, OrderStatus

---
+++ Back-end/src/main/java/com/hung/shop/entity/OrderStatus.java
@@ -0,0 +1,23 @@
+package com.hung.shop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order_status")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderStatusId;
+
+    @Column(nullable = false, unique = true)
+    private String statusName;
+
+}
---
+++ Back-end/src/main/java/com/hung/shop/entity/Orders.java
@@ -0,0 +1,44 @@
+package com.hung.shop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Orders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @Column(nullable = false, unique = true)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private Long orderStatusId;
+
+    @Column(nullable = false)
+    private String address;
+    @Column(nullable = false)
+    private String phone;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
