62133b63f1dfa28ddc30bc17b5fe1e41684e6441
Le Nguyen Hung <lnhungtn789@gmail.com>
2025-04-14 13:26:37 +0700

    update: get user by id

--- Back-end/src/main/java/com/hung/shop/controllers/UserController.java
+++ Back-end/src/main/java/com/hung/shop/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.hung.shop.controllers;
 
 import com.hung.shop.dto.request.UserCreateDto;
 import com.hung.shop.dto.respond.UserDto;
+import com.hung.shop.entity.CustomUserDetails;
 import com.hung.shop.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -11,7 +12,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.security.PermitAll;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -41,9 +45,18 @@ public class UserController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "User found successfully"),
             @ApiResponse(responseCode = "400", description = "Invalid request or user not found"),
+            @ApiResponse(responseCode = "403", description = "Forbidden, current user is not authorized to access other user's information"),
     })
     public ResponseEntity<?> getUserById(@PathVariable Long id) {
         try {
+            // Get the current authenticated user
+            CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+            Long userIdFromToken = userDetails.getUser().getUserId();
+
+            if (!userIdFromToken.equals(id)) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You are not authorized to access other user's information");
+            }
             UserDto userDto = userService.getUserById(id);
             return ResponseEntity.ok(userDto);
         } catch (Exception e) {
