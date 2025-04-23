bc45cd88f43338d7186632bc7ddf8bcb70aeb958
Le Nguyen Hung <lnhungtn789@gmail.com>
2025-04-23 16:16:23 +0700

    add: get Paged Product to FE

--- Front-end/myshop/src/App.tsx
+++ Front-end/myshop/src/App.tsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 import HeroSection from "./components/Hero";
 import FeatureSection from "./components/Feature";
 import CategoryMenu from "./components/CategoryMenu";
-import { fetchAllProducts } from "./api/ProductAPI";
+import { fetchPagedProduct } from "./api/ProductAPI";
 import { Product } from "./types/Product";
 //import { products } from "./dummy/DummyProductList"; 
 import { categories } from "./dummy/DummyCategories";
@@ -13,22 +13,28 @@ import './App.css'
 
 function App() {  
   const [products, setProducts] = useState<Product[]>([]);
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(1);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loadProducts = async () => {
+      setLoading(true);
       try {
-        const data = await fetchAllProducts();
-        setProducts(data);
+        const data = await fetchPagedProduct(page, 6); // for example: 6 products per page
+        setProducts(data.content);
+        setTotalPages(data.totalPages);
       } catch (err) {
-        console.error("Failed to fetch products:", err);
+        console.error("Failed to fetch paged products:", err);
       } finally {
         setLoading(false);
       }
     };
 
     loadProducts();
-  }, []);
+  }, [page]);
+  // const handleNext = () => setPage((p) => Math.min(p + 1, totalPages - 1));
+  // const handlePrevious = () => setPage((p) => Math.max(p - 1, 0));
 
   return (
     <>
--- Front-end/myshop/src/api/ProductAPI.ts
+++ Front-end/myshop/src/api/ProductAPI.ts
@@ -1,7 +1,11 @@
 import axiosInstance from "./AxiosInstance";
 import { Product } from "../types/Product";
-
+import { PaginatedResponse } from "../types/PaginatedResponse";
 export const fetchAllProducts = async (): Promise<Product[]> => {
   const response = await axiosInstance.get("/products");
   return response.data;
 };
+export const fetchPagedProduct = async (page: number, size: number): Promise<PaginatedResponse<Product>> => {
+  const response = await axiosInstance.get(`/products/paged?page=${page}&size=${size}`);
+  return response.data;
+}
